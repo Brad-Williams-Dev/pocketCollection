ad8a29a670adf18c2e0d3db5ca9096a1838fcf31
Brad Williams <bradley_williams2003@hotmail.com>
2023-05-12 09:25:51 -0300

    Add signup and signin screens

---
+++ screens/SignInScreen.jsx
@@ -0,0 +1,123 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import * as Font from "expo-font";
+import LinearGradient from "react-native-linear-gradient";
+import React, { useState, useEffect } from "react";
+
+const SignInScreen = ({ navigation }) => {
+  const [isFontLoaded, setIsFontLoaded] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loadFont = async () => {
+    await Font.loadAsync({
+      "pokemon-font": require("../assets/fonts/pokemon-font.ttf"),
+      // Replace './assets/fonts/pokemon-font.ttf' with the actual path to your font file
+    });
+    setIsFontLoaded(true);
+  };
+
+  useEffect(() => {
+    loadFont();
+  }, []);
+
+  const signIn = () => {
+    // Add your sign in logic here
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text
+        style={
+          isFontLoaded
+            ? { ...styles.title, fontFamily: "pokemon-font" }
+            : styles.title
+        }
+      >
+        Pokémon Card App
+      </Text>
+      <Image
+        style={styles.logo}
+        source={{
+          uri: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png",
+        }}
+      />
+
+      <TextInput
+        style={styles.input}
+        onChangeText={setEmail}
+        value={email}
+        placeholder="Email"
+        keyboardType="email-address"
+        textContentType="emailAddress"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+        placeholder="Password"
+        secureTextEntry
+      />
+      <TouchableOpacity style={styles.button} onPress={signIn}>
+        <Text style={styles.buttonText}>Sign In</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.navigate("SignUp")}>
+        <Text style={styles.signUpText}>Don't have an account? Sign Up</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: "100%",
+    backgroundColor: "#D7853F",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 48,
+    marginBottom: 20,
+    color: "#10717F",
+    transform: [{ rotate: "-10deg" }, { translateY: -20 }],
+  },
+  input: {
+    height: 40,
+    width: "80%",
+    borderColor: "#888",
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingLeft: 10,
+    borderRadius: 10,
+    backgroundColor: "#FFFFFF",
+  },
+  logo: {
+    width: 250,
+    height: 250,
+    marginBottom: 20,
+  },
+  button: {
+    backgroundColor: "#10717F",
+    padding: 10,
+    borderRadius: 10,
+    width: "50%",
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+  },
+  signUpText: {
+    marginTop: 20,
+    color: "#110E0B",
+  },
+});
+
+export default SignInScreen;
